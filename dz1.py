f3f59fdcbfe241059b2bce82b1568cf6e0d2e9fe
Георгий Петрушин <petrushin.gd@edu.spbstu.ru>
2024-10-04 11:30:01 +0300

    test

---
+++ dz1.py
@@ -0,0 +1,17 @@
+'''
+n = int(input())
+if n > 100:
+    print('Верхний предел не должен превышать числа 100!')
+else:
+    num = 0
+    for i in range(1, n+1):
+        num += i ** 3
+    print(num)
+'''
+'''
+num = 9
+for i in range(1, 10):
+    N = i * num
+    print(N)
+'''
+
